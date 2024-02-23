3a42acb9d83bbcedbfebc3d90ca2f3bab2b3c3ec
ntqogly <106591320+ntqogly@users.noreply.github.com>
2024-02-23 22:35:19 +0300

    Revert "Revert "Обработка некорректных введенных данных: числа в названии товара, отр…""

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -7,24 +7,43 @@ public class Calculator {
 
     public Calculator(int numberOfPeople) {
         this.numberOfPeople = numberOfPeople;
-        this.products = new Product[10];
+        this.products = new Product[10]; //Предполагаю, что товаров будет добавлено макс 10 штук. Можно изменить значение в большую сторону
         this.productCount = 0;
     }
 
     public void addProduct(Scanner scanner) {
+        String inputProductName = "";
         System.out.println("Введите название товара:");
-        String productName = scanner.next();
 
-        System.out.println("Введите стоимость товара: ");
-        while (!scanner.hasNextDouble()) {
-            System.out.println("Некорректное значение. Повторите ввод:");
-            scanner.next();
+        while (true) {
+            inputProductName = scanner.next();
+
+            if (inputProductName.matches(".*\\d.*")) {
+                System.out.println("Некорректное наименование. Повторите ввод:");
+            } else {
+                break;
+            }
         }
-        double productPrice = scanner.nextDouble();
+
+        double inputProductPrice = 0.0;
+        System.out.println("Введите стоимость товара: ");
+        while (true) {
+            while (!scanner.hasNextDouble()) {
+                System.out.println("Некорректное значение. Повторите ввод:");
+                scanner.next();
+            }
+            inputProductPrice = scanner.nextDouble();
+            if (inputProductPrice <= 0) {
+                System.out.println("Некорректное значение. Повторите ввод:");
+            } else {
+                break;
+            }
+        }
 
         if (productCount < products.length) {
-            products[productCount++] = new Product(productName, productPrice);
-            System.out.println("Товар добавлен: " + productName + " (стоимость: " + Formatter.formatCurrency(productPrice) + ")");
+            products[productCount] = new Product(inputProductName, inputProductPrice);
+            productCount++;
+            System.out.println("Товар добавлен: " + inputProductName + " (стоимость: " + Formatter.formatCurrency(inputProductPrice) + ")");
         }
     }
 
@@ -43,5 +62,20 @@ public class Calculator {
         double billPerPerson = totalBill / numberOfPeople;
 
         System.out.println("Сумма на одного человека: " + Formatter.formatCurrency(billPerPerson));
+    }
+
+    public boolean addNewProduct(Scanner scanner) {
+        System.out.println("Введите: 'Продолжить' для добавления нового товара 'Завершить' для завершения");
+
+        do {
+            String inputEndOrContinue = scanner.next().toLowerCase();
+            if (inputEndOrContinue.equals("завершить")) {
+                return true;
+            } else if (inputEndOrContinue.equals("продолжить")) {
+                return false;
+            } else {
+                System.out.println("Некорректный ввод. Введите: 'Продолжить' для добавления нового товара 'Завершить' для завершения");
+            }
+        } while (true);
     }
 }
